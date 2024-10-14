baa7910d560a4a66fae86ad35de9043c14e745d8
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-10-14 11:50:38 +0000

    メンター4

--- db/migrate/20241010151630_change_category_to_integer_in_spaces.rb
+++ db/migrate/20241010151630_change_category_to_integer_in_spaces.rb
@@ -1,6 +1,6 @@
 class ChangeCategoryToIntegerInSpaces < ActiveRecord::Migration[6.1]
   def change
-    remove_column :spaces, :category, :string # まず、古いカラムを削除
-    add_column :spaces, :category, :integer, null: false, default: 0 # 新しいカラムを追加
+
+    change_column :spaces, :category, :integer, null: false, default: 0 # 新しいカラムを追加
   end
 end
