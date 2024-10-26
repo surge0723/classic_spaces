0c504225fe43ce9d501b18ec1dd9786acbf34cb1
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-10-26 08:10:09 +0000

    admincomment clear

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -8,6 +8,6 @@ class Admin::DashboardsController < ApplicationController
     def show
     @user = User.find(params[:id])
     @spaces_list = @user.spaces
-    @space_comment = @user.comments
+    @spaces_comment = @user.comments.includes(:space) 
     end
 end
